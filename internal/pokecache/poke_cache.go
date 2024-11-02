b8b44790bd7ddd55fdec7cee1c521326017ebd93
Dmitrij Patuk <dmitrij.patuk@gmx.de>
2024-11-02 16:09:24 +0100

    cache skeleton created

--- internal/pokecache/poke_cache.go
+++ internal/pokecache/poke_cache.go
@@ -1,1 +1,27 @@
 package pokecache
+
+import "time"
+
+type CacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	entries  map[string]CacheEntry
+	interval time.Duration
+}
+
+func (c *Cache) NewCache() {
+}
+
+func (c *Cache) Add(key string, val []byte) {
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	return nil, false
+}
+
+func (c *Cache) reapLoop() {
+
+}
